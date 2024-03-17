abf4bd009f8c63cbd9c81104116d90402dcefa20
Paul Lorber <paul.lorber@protonmail.com>
2024-03-17 21:34:45 +0100

    uncaughtpokemons file and some styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,36 @@
-function Landing () {
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+
+import UncaughtPokemons from './UncaughtPokemons';
+import MyPokemons from './MyPokemons';
+
+import style from '../styles/Landing.module.css';
+
+
+function Landing() {
+
+  const fetchData = async () => {
+    const response = await fetch('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json');
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  } , []);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <>
+      <nav className={style.nav}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/pokedex">Pokedex</NavLink>
+        <NavLink to="leaderboard">Leaderboard</NavLink>
+        <button>🌑</button>
+      </nav>
+      <MyPokemons />
+      <UncaughtPokemons />
+    </>
+  );
 }
 
-export default Landing
+export default Landing;
