4e413422d31e43d5cd19d4bebef7cb637ffb74c7
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-03-18 10:21:16 +0100

    add authentification wall

---
+++ src/components mathieu/MainContent.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const MainContent = ({ username }) => {
+  return (
+    <div>
+      <h2>Welcome to the Pokemon World, {username}!</h2>
+      <p>POkeshits here</p>
+    </div>
+  );
+};
+
+export default MainContent;
