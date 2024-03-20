356e64a53bd85f2e1dc044e7e4773b71cfc7ea90
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-03-20 22:30:03 +0100

    battle rounds logic

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -9,33 +9,64 @@ export default function Battle() {
   const charmander = pokemon[3];
   const [userPokemon, setUserPokemon] = useState(null);
   const [opponentPokemon, setOpponentPokemon] = useState(null);
+  const [userCount, setUserCount] = useState(5);
+  const [opponentCount, setOpponentCount] = useState(5);
 
   //console log without repetitions
   useEffect(() => {
     if (!loading) {
-      console.log("Data in battle component:", pokemon[1]);
+      console.log("Data in battle component:", pokemon[1].base.Attack);
     }
   }, [pokemon, loading]);
 
-  //Set user pokemon (este setter cambia después)
+  //Set user pokemon (This setter will change)
   useEffect(() => {
     if (ivysaur) {
       setUserPokemon(ivysaur);
     }
   }, [ivysaur]);
 
-  //Set opponent pokemon (este setter cambia después)
+  //Set opponent pokemon (This setter will change)
   useEffect(() => {
     if (charmander) {
       setOpponentPokemon(charmander);
     }
   }, [charmander]);
 
+  //Handle click
+  function handleClick() {
+    //If userpokemon attack > opponentpokemon attack setUserCount (count - 1)
+    function attackRound() {
+      if (userPokemon.base.Attack >= opponentPokemon.base.Attack) {
+        setOpponentCount(opponentCount - 1);
+        //aquí meter css verde para style.base
+      } else {
+        setUserCount(userCount - 1);
+        //aquí meter css rojo para style.base
+      }
+    }
+    //Same but for defense + 1sec delay
+    attackRound();
+    function defenseRound() {
+      setTimeout(() => {
+        if (userPokemon.base.Defense >= opponentPokemon.base.Defense) {
+          setOpponentCount(opponentCount - 1);
+        } else {
+          setUserCount(userCount - 1);
+        }
+      }, 1000);
+    }
+    defenseRound();
+    //Same but for Sp Attack +2sec delay
+    function spAttackRound() {}
+  }
+
   return (
     <div className={styles.battle_component}>
       <h1 className={styles.title}>Battle</h1>
       <div className={styles.battle_container}>
         <div className={styles.pokemon_box}>
+          <h3>HP: {userCount}/5</h3>
           <h2>{userPokemon && userPokemon.name.japanese}</h2>
           <img
             src={userPokemon && userPokemon.image.thumbnail}
@@ -50,6 +81,7 @@ export default function Battle() {
           <div className={styles.base}>Speed</div>
         </div>
         <div className={styles.pokemon_box}>
+          <h3>HP: {opponentCount}/5</h3>
           <h2>{opponentPokemon && opponentPokemon.name.japanese}</h2>
           <img
             src={opponentPokemon && opponentPokemon.image.thumbnail}
@@ -58,7 +90,9 @@ export default function Battle() {
         </div>
       </div>
       <div className={styles.btn_container}>
-        <button className={styles.fight_btn}>Fight!</button>
+        <button className={styles.fight_btn} onClick={handleClick}>
+          Fight!
+        </button>
       </div>
     </div>
   );
