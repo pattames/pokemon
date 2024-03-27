04763641ecc11ac13df65678ba62bf6e65d168d6
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-03-27 18:07:07 +0100

    presentation material

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,8 @@
+import React from "react";
 
-import React from 'react';
-
-const MainContent = ({ username }) => { // Receive username prop
-  return (
-    <div>
-      <h2>Welcome to the Pokemon World, {username}!</h2>
-    </div>
-  );
+const MainContent = ({ username }) => {
+  // Receive username prop
+  return <div>{/* <h2>Welcome to the Pokemon World, {username}!</h2> */}</div>;
 };
 
 export default MainContent;
